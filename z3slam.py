43dd796cda79234b3dff09ab64fe68b95e4c83db
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2022-04-30 15:27:54 +0200

    Update z3slam.py

--- z3slam.py
+++ z3slam.py
@@ -21,7 +21,7 @@ python3 {__file__} ip port bytes
 try:
     websiteHost_or_IP = sys.argv[1]
     portX = int(sys.argv[2])
-    bytesX = int(sys.argv[2])
+    bytesX = int(sys.argv[3])
     
 except:
     Usage()
